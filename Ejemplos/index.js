297635f89c4e742cf7bfb788536fd8cd7f7f56b2
latecnologiaavanza <cramirezchristian77@gmail.com>
2025-11-11 05:53:03 -0500

    commit introduccion middlewares

--- Ejemplos/index.js
+++ Ejemplos/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
-//Agrega un middleware a la aplicación Express
-//Un middleware es una función que se ejecuta antes de que lleguen las peticiones a tus rutas
-//En este caso, se usa para procesar el cuerpo (body) de las solicitudes HTTP
-//El parámetro "next" también es una función e indica que ya terminó el trabajo
+app.use(morgan('dev'))
+
 app.use((req, res, next) => {
   console.log(`Route: ${req.url} - Metodo: ${req.method}`);
   next();
@@ -23,6 +22,17 @@ app.all("/about", (req, res) => {
   res.send("About Page");
 });
 
+app.use((req, res, next) => {
+  if (req.query.login === "christian") {
+    next();
+  } else {
+    res.send("No Autorizado");
+  }
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("Dashboard Page");
+});
 
 app.listen(3000);
 console.log(`Server on Port ${3000}`);
