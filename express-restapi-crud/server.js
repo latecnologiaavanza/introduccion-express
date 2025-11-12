eda93cbef3802d042048a8b948f2784d1cd2873f
latecnologiaavanza <cramirezchristian77@gmail.com>
2025-11-12 06:51:42 -0500

    commit rest api

--- express-restapi-crud/server.js
+++ express-restapi-crud/server.js
@@ -2,15 +2,31 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+const productos = [
+  {
+    id: 1,
+    nombre: "Laptop",
+    precio: 5000,
+  },
+  {
+    id: 2,
+    nombre: "IPhone",
+    precio: 7000,
+  },
+];
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/productos", (req, res) => {
-  res.send("Obteniendo Productos");
+  res.json(productos);
 });
 
 app.post("/productos", (req, res) => {
-  res.send("Creando Productos");
+  //El operador spread (...) copia todas las propiedades de un objeto dentro de otro
+  const newProducto = { ...req.body, id: productos.length + 1 };
+  productos.push(newProducto);
+  res.json(newProducto);
 });
 
 app.put("/productos", (req, res) => {
@@ -22,7 +38,13 @@ app.delete("/productos", (req, res) => {
 });
 
 app.get("/productos/:id", (req, res) => {
-  res.send("Obteniendo un Producto");
+  const productoFind = productos.find((p)=> p.id === parseInt(req.params.id));
+
+  if (!productoFind) {
+    return res.status(404).json({ message: "Produco no encontrado" });
+  }
+
+  res.json(productoFind);
 });
 
 app.listen(3000);
